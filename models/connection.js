6ba1d81439c23f24282508c148d7ffe653b808ad
maiky5198 <maiky5198@gmail.com>
2022-03-22 10:21:48 -0400

    update for deployment

--- models/connection.js
+++ models/connection.js
@@ -6,16 +6,24 @@ const mongoose = require('mongoose')
 
 /////////////////////////////////
 // database connection
+// local database connection => process.env.DATABASE_URL
+// remote database connection => process.env.MONGODB_URI
 /////////////////////////////////
 // here we are setting up inputs for our connect function
-const DATABASE_URL = process.env.DATABASE_URL
+const MONGODB_URI = process.env.MONGODB_URI
 const CONFIG = {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }
-
+mongoose.connection.on('connected', () => {
+	console.log(`Mongoose connected to ${mongoose.connection.host}:${mongoose.connection.port}`);
+  });
+  
+  mongoose.connection.on("error", (err) => {
+	console.log("Could not connect to MongoDB!", err);
+  });
 // establish connection
-mongoose.connect(DATABASE_URL, CONFIG)
+mongoose.connect(MONGODB_URI, CONFIG)
 
 // events for when our connection opens/closes/errors
 mongoose.connection
